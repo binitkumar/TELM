6409cafef488f4035ad1a42e350265cee9d9f906
Binit Kumar <binit.kumar.hbs@gmail.com>
2012-02-14 11:57:33 -0800

    With changes for Heroku

--- app/controllers/study_material_controller.rb
+++ app/controllers/study_material_controller.rb
@@ -2,4 +2,9 @@ class StudyMaterialController < ApplicationController
 	def index
 		@batches = Batch.active
 	end
+	
+	def list_uploads_by_course
+		@students = Student.find_all_by_batch_id(params[:batch_id], :order => 'first_name ASC')
+		render(:update) { |page| page.replace_html 'students', :partial => 'uploads_by_course' }
+	end
 end
